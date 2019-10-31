23db01ad850ee5b08c182ff147314253974be888
reidaruss <reidarussell@gmail.com>
2019-10-31 18:00:34 -0500

    Added Login and registration for HR and employee

---
+++ labgui/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import LoginScreen from './Loginscreen';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      uploadScreen:[]
+    }
+  }
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<LoginScreen appContext={this} key={"login-screen"}/>);
+    this.setState({
+                  loginPage:loginPage
+                    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.loginPage}
+        {this.state.uploadScreen}
+      </div>
+    );
+  }
+}
+
+export default App;
