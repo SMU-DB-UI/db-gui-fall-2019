d37f346334fc24636bcb3af5850e855863180143
= <nathanhites@yahoo.com>
2019-11-09 18:40:00 -0600

    skeleton for get and post done

--- DB/src/perf_reviews/perf_reviews.controller.js
+++ DB/src/perf_reviews/perf_reviews.controller.js
@@ -14,17 +14,31 @@ function notLoggedIn(req, res) {
   }
 
 //Must be logged in to add a new performance review to an employee
-router.post('/employees/:empId/profile/performance_reviews'), async (req, res) => {
+router.post('/employees/:empId/profile/performance_reviews', async (req, res) => {
 
     if (notLoggedIn(req, res)) return;
 
     let {connection, message} = await conn.getConnection(res);
     if (message == 'fail') return;
 
-    let response = model.postNewPerfRev(connection, req.params.empId, req.body);
+    let response = await model.postNewPerfRev(connection, req.params.empId, req.body);
     res.json(response);
 
-}
+});
+
+router.get('/employees/:empId/profile/performance_reviews', async (req, res) => {
+  let {connection, message} = await conn.getConnection(res);
+  if (message == 'fail') return;
+
+  let perf_rev = await model.seeAllPerfRevs(connection, req.params.empId);
+  res.json({
+    id:           perf_rev.id, 
+    emp_id_refs:  perf_rev.emp_id,
+    text:         perf_rev.review,
+    score:        perf_rev.score,
+    date_created: perf_rev.creation_date
+  });
+});
 
 
 module.exports = router;
--- DB/src/perf_reviews/perf_reviews.model.js
+++ DB/src/perf_reviews/perf_reviews.model.js
@@ -4,7 +4,7 @@ var logger = require('../helpers/logger');
 async function postNewPerfRev(connection, empId, body) {
     try {
       await connection.query(`INSERT INTO perf_reviews (emp_id, review, score, creation_date) VALUES
-      (${empId}, ${body.review}, ${body.score}, ${body.creation_date})`);
+      (${empId}, '${body.review}', ${body.score}, ${body.creation_date})`);
     }
     catch (e) {
       logger.error(e);
@@ -14,8 +14,21 @@ async function postNewPerfRev(connection, empId, body) {
     return {message: 'succeed'};
   }
 
+async function seeAllPerfRevs(connection, empId) {
+  try {
+    await connection.query(`SELECT * FROM perf_reviews WHERE emp_id = ?`, [empId]);
+  }
+  catch (e) {
+    logger.error(e);
+    return {message: 'fail'};
+  }
+
+  return {message: 'succeed'};
+}
+
 
 module.exports = {
     //functions that will be used
     postNewPerfRev
+    seeAllPerfRevs
   };
