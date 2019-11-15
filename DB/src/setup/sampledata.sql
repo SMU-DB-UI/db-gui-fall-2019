ab724e4512385ce404e88c3c80d80378160b978c
lizzyhanna <lhanna@smu.edu>
2019-11-14 20:31:19 -0600

    Automatically updates an employee's score whenever their profile is viewed

--- DB/src/setup/sampledata.sql
+++ DB/src/setup/sampledata.sql
@@ -1,8 +1,8 @@
 INSERT INTO departments VALUES(1, 'Computer Science');
 
 INSERT INTO employees VALUES
-    (1, 'mark', 'fontenot', 1, 'prof', null, 'address', 'email1', 214, 0, 0, null, null, null, 'true'),
-    (2, 'Marcus', 'Sykora', 1, 'Student', '1', 'address', 'email2', 214, 0, 0, null, null, null, 'true');
+    (1, 'mark', 'fontenot', 1, 'prof', null, 'address', 'email1', 214, -1, 0, null, null, null, 'true'),
+    (2, 'Marcus', 'Sykora', 1, 'Student', '1', 'address', 'email2', 214, -1, 0, null, null, null, 'true');
 
 INSERT INTO reports VALUES
     (1, 1, 2, 'Test report text', '11/5/2019', 'open', null, 0),
