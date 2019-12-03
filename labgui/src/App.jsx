70c06ea6d09a4dcf9eb3135ce7ccf42e99815ebf
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2019-12-02 21:49:44 -0600

    Added ynamic title, separated routes, and made links close nav

--- labgui/src/App.jsx
+++ labgui/src/App.jsx
@@ -11,20 +11,60 @@ import {Header} from './app/header'
 class App extends Component {
 
   state = {
-    navigating: false
+    navigating: false,
+    hrAuth: false,
+    empAuth: true,
   }
 
   toggleNav() {
     this.setState({navigating: !this.state.navigating})
   }
+
+  getRoutes() {
+    if (this.state.empAuth) {
+      return (
+        Routes().employee.map(x => (
+          <Route exact path={x[0]} component={x[1]} key={x[0]}></Route>
+        ))
+      )
+    }
+    else if (this.state.hrAuth) {
+      return (
+        Routes().hr.map(x => (
+          <Route exact path={x[0]} component={x[1]} key={x[0]}></Route>
+        ))
+      )
+    }
+  }
+
+  getNavLinks() {
+    if (this.state.empAuth) {
+      return (
+        Routes().employee.map(x => (
+              <Link to={x[0]} className='text-light' onClick={() => this.toggleNav()} key={x[0]}>
+                  <h3 className='ml-3'>{x[2]}</h3>
+              </Link>
+        ))
+      )
+  }
+  else if (this.state.hrAuth) {
+    return (
+        Routes().hr.map(x => (
+          <Link to={x[0]} className='text-light' onClick={() => this.toggleNav()} key={x[0]}>
+              <h3 className='ml-3'>{x[2]}</h3>
+          </Link>
+        ))
+      )
+  }
+  }
   constructor(props){
     super(props);
-    this.state={
-      loginPage:[],
-      uploadScreen:[],
-      reportsPage: [],
-      reviewsPage: []
-    }
+    // this.state={
+    //   loginPage:[],
+    //   uploadScreen:[],
+    //   reportsPage: [],
+    //   reviewsPage: []
+    // }
   }
   componentWillMount(){
     var reports = [];
@@ -44,27 +84,16 @@ class App extends Component {
   }
   render() {
     return (
-      
       <div className="App">
         <BrowserRouter>
             <Header toggleNav={x => (this.toggleNav())}></Header>
             <div className='col-2 align-left float-left card text-left jumbotron jumbotron-fluid bg-info maxHeight' 
                 style={{'display': (this.state.navigating ? 'block' : 'none')}}>
-                    {
-                        Routes().map(x => (
-                              <Link to={x[0]} className='text-light'>
-                                  <h3 className='ml-3'>{x[2]}</h3>
-                              </Link>
-                        ))
-                    }
+                    {this.getNavLinks()}
             </div>
             <div className={(this.state.navigating ? 'col-10 float-right' : '')}>
-                <Switch>
-                {
-                  Routes().map(x => (
-                    <Route exact path={x[0]} component={x[1]} key={x[0]}></Route>
-                  ))
-                }
+                <Switch>           
+                {this.getRoutes()}
                 </Switch>
             </div>
             {/* {this.state.loginPage}
